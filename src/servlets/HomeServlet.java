b492c1150c402e631d41d6e11a9529c99f69d2b2
Dimash <d.esengeldiev@mail.ru>
2025-01-09 11:23:30 +0500

    init

---
+++ src/servlets/HomeServlet.java
@@ -0,0 +1,21 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(value = "/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        for (int i = 0; i < 10; i++) {
+            out.println("<h1>Hello world</h1>");
+        }
+    }
+}
